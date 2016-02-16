bde97e6a456b72c5b61169a4d214025fc3fd5089
Aaron Windsor <aaron.windsor@gmail.com>
2016-02-16 09:41:46 -0500

    Store app environment instead of relying on .aptible.env (Fixes #2)

--- src/run-cron.sh
+++ src/run-cron.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CRON_SCHEDULE=${CRON_SCHEDULE:-"0 2 * * *"}
 sed "s:CRON_SCHEDULE:${CRON_SCHEDULE}:g" /opt/app/src/backup.crontab.template > /opt/app/src/backup.crontab
+env > /opt/app/src/app.env
 crontab /opt/app/src/backup.crontab
 touch /var/log/cron.log
 crond
